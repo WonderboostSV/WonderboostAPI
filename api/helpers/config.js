29c3aee7d464fd5e88aa822d263f5171db8cc6eb
Juan Pablo Flores Díaz <101837425+JuanPabloFloresDiaz@users.noreply.github.com>
2024-11-27 00:18:21 -0600

    Documentación segunda parte

--- api/helpers/config.js
+++ api/helpers/config.js
@@ -16,7 +16,7 @@ const config = {
     database: process.env.DB_DATABASE,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    dialect: process.env.DB_DIALECT// Agrega el dialecto de la base de datos
+    dialect: process.env.DB_DIALECT
 };
 
 // Encabezado para permitir solicitudes de cualquier origen (si se implementa un servidor HTTP más adelante)
